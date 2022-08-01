1129855a4ee6f77715c26a372ffe88010c43bf48
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-08-01 23:57:41 +0200

    create ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+// import ContactList from './ContactList/ContactList';
+
+import { nanoid } from 'nanoid'
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    filter: '',
+  }
+  formSubmitHandler = data => {
+    console.log(data);
+  }
+  render() {
+    return (
+      <>
+  <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+  <h2>Contacts</h2>
+  {/* <Filter ... /> */}
+  {/* <ContactList ... /> */}
+</>
+    );
+  }
 };
